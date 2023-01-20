362668a35648b168f3697c8946686a1bc4d96000
Salman Shaikh <salmanshanu@SALMANs--iMac.hsd1.ga.comcast.net>
2023-01-20 18:52:49 -0500

    hello-1

---
+++ src/Sample.java
@@ -0,0 +1,8 @@
+
+// i am creating my own project from (local)
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("hello-project from local");
+    }
+
+}
